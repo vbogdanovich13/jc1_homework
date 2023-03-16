a773ba6c5a7c05886d2e3c5317e9a3108eb2fbed
vbogdanovich13 <vbogdanovich13@gmail.com>
2023-03-16 21:24:02 +0300

    added first homework

---
+++ src/part1/task1/HelloWorldTime.java
@@ -0,0 +1,19 @@
+package src;
+
+public class HelloWorldTime {
+    public static void main(String[] args) {
+        int s = 1914400;
+        int sec;
+        int m;
+        int min, h, week, day, d, hour;
+        sec = s % 60;
+        m = (s - sec) / 60;
+        min = m % 60;
+        h = (m - min) / 60;
+        hour = h % 24;
+        d = (h - hour) / 24;
+        day = h % 24;
+        week = d / 7;
+        System.out.println(week + " недель " + day + " суток " + hour + "  часов " + min + " минут " + sec + " секунд");
+    }
+}

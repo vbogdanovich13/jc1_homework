b4204d49a949ad4cfe679dbd923f46e44fb20c54
vbogdanovich13 <99118878+vbogdanovich13@users.noreply.github.com>
2023-04-04 00:31:28 +0300

    corrected task 14

--- src/part4/task14/Task14.java
+++ src/part4/task14/Task14.java
@@ -1,13 +1,19 @@
 package part4.task14;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class Task14 {
     public static void main(String[] args) {
-        long s = 1L;
-        int i = 1;
+        BigInteger s = BigInteger.ONE;
+        BigInteger a = BigInteger.ONE;
+        BigInteger b = BigInteger.ONE;
+        BigInteger exit = new BigInteger("26");
+
         do {
-            s *= i;
-            i++;
-        } while (i >= 1 && i <= 25);
+            s = s.multiply(a);
+            a = a.add(b);
+        } while (!a.equals(exit));
         System.out.println("Произведение чисел от 1 до 25 равно: " + s);
     }
 }
